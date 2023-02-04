aa32095181e7fef24fcdd72c4d47ca9a098b5485
Yudai Nakajima <kituneudon97@gmail.com>
2023-02-04 16:51:18 +0900

    コンストラクトを書くを終えた

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,7 +2,7 @@ import { Duration, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,8 +14,12 @@ export class CdkWorkshopStack extends Stack {
       handler: 'hello.handler'
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
   }
 }
